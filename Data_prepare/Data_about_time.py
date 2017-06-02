c9b7996e37b68f98c57c2570744a66e27c6899ac
GuHong <kuhung@foxmail.com>
2017-06-02 17:37:47 +0800

    Update Data_about_time.py

--- Data_prepare/Data_about_time.py
+++ Data_prepare/Data_about_time.py
@@ -1,2 +1,6 @@
+# Author：kuhung （https://github.com/kuhung）
+# Create time：2017/06/02
+# Source: https://github.com/kuhung/Student-Grants/blob/master/kuhung/base_line/feature-2-time.py
+
 
 card_train_test.time = pd.to_datetime(card_train_test.time, format='%Y/%m/%d %H:%M:%S')
