b730de80b7d0a5bcfc4ccacb2b462e8550c51a69
GuHong <kuhung@foxmail.com>
2017-06-05 16:23:26 +0800

    Create xgboost_cv.py

---
+++ Model/xgboost_cv.py
@@ -0,0 +1,20 @@
+
+
+
+
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+
+labels = train["label"]
+
+features = train.drop(['label','id'], axis=1)
+data_dmat = xgb.DMatrix(data=features, label=labels)
+
+
+params={}
+
+rounds = 50
+
+result = xgb.cv(params=params, dtrain=data_dmat, num_boost_round=rounds, early_stopping_rounds=5, as_pandas=True, seed=2333)
+print result
